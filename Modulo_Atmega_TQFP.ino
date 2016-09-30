62aba93d4539e05ea3794b3d1f93b3256a51b2bd
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2016-09-30 16:28:41 -0300

    fix error
    funciona bien sin eerror al resetqar actuador

--- Modulo_Atmega_TQFP.ino
+++ Modulo_Atmega_TQFP.ino
@@ -41,6 +41,8 @@ MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
 void setup()
 {
+
+    
     Serial.begin(9600);
 
   // EEPROM.begin(0x1024);
@@ -56,7 +58,6 @@ void setup()
     MsgUpEEprom[1]=ID_Master;
     
     pinMode(LED,OUTPUT);
-   digitalWrite(LED,true);
     pinMode(Relay_1,OUTPUT);
     digitalWrite(Relay_1,true);
     pinMode(Relay_2,OUTPUT);
@@ -66,18 +67,18 @@ void setup()
 // PRUEBA INICIO RELAYS //
     digitalWrite(Relay_1,false);
     digitalWrite(LED,false);
-    delay(200);
+    delay(100);
     digitalWrite(Relay_1,true);
     digitalWrite(LED,true);
-    delay(200);
+    delay(100);
     digitalWrite(Relay_2,false);
     digitalWrite(LED,false);
-    delay(200);
+    delay(100);
     digitalWrite(Relay_2,true);
     digitalWrite(LED,true);
        
     pinMode(interrupcion ,INPUT);
-
+    delay(300);
    // ID_Local=EEPROM.read(0x00);
   //  ID_Master=EEPROM.read(0x01);
   
@@ -103,13 +104,9 @@ void loop()
 
 
       
-    if(!digitalRead(interrupcion))            // check if data coming
-    
-    {   
+    if(!digitalRead(interrupcion)){   
 
-////////////////////////////////////    
-      Led_Blink(1);
-      
+  
       // noInterrupts();               // Suspende las interrupciones
           CAN.readMsgBuf(&len, buf);    // leo el mensaje recibido
           canId = CAN.getCanId();       // almaceno el Id del emisor
@@ -121,20 +118,11 @@ void loop()
           MsgLeido[3]=buf[3];MsgLeido[2]=buf[2];MsgLeido[1]=buf[1];MsgLeido[0]=buf[0];
 
         if( canId==0x00){// MAster  broadcast 0xFF para que todos los ID LOCALEs publiquen su info
-          //  Led_mensaje_recibido_blink();
-          
-////////////////////////////////////    
- 
-         Led_Blink(2);
+
              if(MsgLeido[0]==0xFF){
-         
-////////////////////////////////////    
- 
-         Led_Blink(3);      
+   
             if(MsgLeido[7]==ID_Local){
- 
-////////////////////////////////////    
-         Led_Blink(4);
+
               EEPROM.write(0x00, MsgLeido[6]);// escribe en la dir 0x00 el id del dispositivo LOCAL
               EEPROM.write(0x01, MsgLeido[5]);// escribe en la dir 0x01 el id del MASTER controlador
               ID_Local= EEPROM.read(0x00);    // almaceno el Id del receptor
@@ -142,33 +130,24 @@ void loop()
               MsgUpEEprom[0]=ID_Local;
               MsgUpEEprom[1]=ID_Master;
               CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
-        //      Led_grabacion_3();}
+            //  Led_grabacion_3();
+        }
             }
            }
        
        if( canId==0xFF){// MAster  broadcast 0xFF para que todos los ID LOCALEs publiquen su info
-        //   Led_mensaje_recibido_blink();
-       Led_Blink(5);
-           CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
+            CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
              
         
       }
        else{        
             if( canId == ID_Master){  // su el Master coinside con el emisor
-             // Led_mensaje_recibido_blink();
-              
-////////////////////////////////////    
-         Led_Blink(6);
-    
+             Led_Blink(1);
               if(0x00==MsgLeido[0]){    //si el control es 00 solo envia su info el local
+                
                  CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
-                  
-////////////////////////////////////    
-         Led_Blink(7);
-    
-                 }
+                   }
               if(MsgLeido[0]==0x01){  
-              
                 if(MsgLeido[1]==0x00){
                   digitalWrite(Relay_1,true);
                   digitalWrite(Relay_2,true);}
@@ -181,34 +160,36 @@ void loop()
                 if(MsgLeido[1]==0x11){
                   digitalWrite(Relay_1,false);
                   digitalWrite(Relay_2,false); }
-             //   CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
+                CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
               
                 }
             }
 
            if(MsgLeido[0]==0xFF){
-             
-////////////////////////////////////    
-         Led_Blink(4);
-              if(MsgLeido[7]==ID_Local){
-              EEPROM.write(0x00, MsgLeido[6]);// escribe en la dir 0x00 el id del dispositivo LOCAL
-              EEPROM.write(0x01, MsgLeido[5]);// escribe en la dir 0x01 el id del MASTER controlador
-              ID_Local= EEPROM.read(0x00);    // almaceno el Id del receptor
-              ID_Master= EEPROM.read(0x01);
-              MsgUpEEprom[0]=ID_Local;
-              MsgUpEEprom[1]=ID_Master;
-              CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
-              //Led_grabacion_3();}
+             if(MsgLeido[7]==ID_Local){
+               EEPROM.write(0x00, MsgLeido[6]);// escribe en la dir 0x00 el id del dispositivo LOCAL
+               EEPROM.write(0x01, MsgLeido[5]);// escribe en la dir 0x01 el id del MASTER controlador
+               ID_Local= EEPROM.read(0x00);    // almaceno el Id del receptor
+               ID_Master= EEPROM.read(0x01);
+               MsgUpEEprom[0]=ID_Local;
+               MsgUpEEprom[1]=ID_Master;
+               CAN.sendMsgBuf(ID_Local,0,8,MsgUpEEprom);
+               Led_grabacion_3();
+              }
             }
-     
+   
        }
       //  interrupts();                 // Autoriza las interrupciones
+
       }
         
-    }
+
+}
+    
 
 
  void Led_grabacion_3(){
+  
           digitalWrite(LED,false);
           delay(200);
           digitalWrite(LED,true);
@@ -220,28 +201,16 @@ void loop()
           digitalWrite(LED,false);
           delay(1000);
           digitalWrite(LED,true);
- 
   }
 
- void Led_mensaje_recibido_blink(){
-          digitalWrite(LED,false);
-          delay(200);
-          digitalWrite(LED,true);  
-          delay(200);
-  }
+ 
+  
  void Led_Testigo(){
           digitalWrite(LED,false);
           delay(100);
           digitalWrite(LED,true);  
           delay(100);
-          digitalWrite(LED,false);
-          delay(100);
-          digitalWrite(LED,true);  
-          delay(100);
-          digitalWrite(LED,false);
-          delay(100);
-          digitalWrite(LED,true);  
-          delay(100);
+      
   }
 
  void Led_0xFF_CanID(){
@@ -307,19 +276,20 @@ void Consumo_ACS712() {
 
 //////////////////////////////////// prueba test
 
-/*
+
 
  void Led_Blink(int c){
 
-    for(int i=0;i=c;i++){
+    for(int i=0;i<c;i++){
+           delay(200);
            digitalWrite(LED,false);
-           delay(500);
+           delay(200);
            digitalWrite(LED,true);
-           delay(2000);
+          
     }
   }
 
-*/
+
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 /*********************************************************************************************************
   END FILE
